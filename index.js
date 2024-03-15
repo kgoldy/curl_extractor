b0290e5a37e8edc48fb621e58e643bead55f1c8b
kgoldy <84913873+kgoldy@users.noreply.github.com>
2024-03-15 08:19:48 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const data = {
-    "curlRequest": "curl -X POST https://example.com/api/path/uuid/subpath --data-raw '{"key": "value"}'",
-}
 
-function extractDataRaw(curlString) {
+export function extractDataRaw(curlString) {
     const dataRawRegex = /--data-raw '([^']*)'/;
 
     const matches = dataRawRegex.exec(curlString);
@@ -14,20 +11,13 @@ function extractDataRaw(curlString) {
     }
 }
 
-function extractUUID(curlString) {
+export function extractUUID(curlString) {
     const uuidRegex = /path\/([^\/]+)\/subpath/;
     const matches = uuidRegex.exec(curlString);
 
     if (matches && matches.length > 1) {
         return matches[1];
     } else {
-        return null; 
+        return null;
     }
 }
-
-
-const curlString = data.curlRequest
-const dataRaw = extractDataRaw(curlString);
-const uuid = extractUUID(curlString);
-console.log(dataRaw);
-console.log(orderRefId)
